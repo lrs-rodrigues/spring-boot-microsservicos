d94668c4656f74e0305845ea4ff85a47711de6ab
Lucas Rodrigues <contato.lucasrodrigues99@gmail.com>
2021-01-10 19:48:45 -0300

    Added new services: Order and Checkout

---
+++ services/checkout/src/test/java/com/lrsrodrigues/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrsrodrigues.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
